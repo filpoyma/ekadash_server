fd4fce064af80ba63c71ed015204c57344527a28
rom <filpoyma@gmail.com>
2026-06-08 17:24:52 +0800

    cities and contries

--- config/appVersions.config.js
+++ config/appVersions.config.js
@@ -1,18 +1,18 @@
 const appVersions = {
   ios: {
-    minVersion: '1.0.8',
-    latestVersion: '1.0.8',
-    storeUrl: '',
+    minVersion: '1.0.5',
+    latestVersion: '1.0.5',
+    storeUrl: ''
   },
   android: {
     minVersion: '1.0.8',
     latestVersion: '1.0.8',
-    storeUrl: 'market://details?id=com.ekadash',
+    storeUrl: 'market://details?id=com.ekadash'
   },
   message: {
     ru: 'Доступна новая версия приложения. Обновите, чтобы продолжить пользоваться всеми функциями.',
-    en: 'A new version of the app is available. Please update to continue using all features.',
-  },
+    en: 'A new version of the app is available. Please update to continue using all features.'
+  }
 };
 
 export default appVersions;
